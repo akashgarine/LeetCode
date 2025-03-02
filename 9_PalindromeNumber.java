5b5624495bc08a1d6240b44599b2cf39c088b6f0
akashgarine <122369528+akashgarine@users.noreply.github.com>
2025-03-02 10:51:16 +0530

    Create 9_PalindromeNumber.java

---
+++ 9_PalindromeNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int sum=0,r;
+        int temp=x;
+    if(x<0){
+         return false;
+    }
+    while(x>0){
+         r=x%10;
+         sum=((sum*10)+r);
+         x=x/10;
+     }
+     if(sum==temp){
+         return true;
+     }
+     else{
+         return false;
+     }
+    }
+}
